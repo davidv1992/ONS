f94eb318bf1619a33992106b511f2550bb01b7b3
David Venhoek <david@venhoek.nl>
2017-10-20 20:54:04 +0200

    Small change to test program.

--- tests/vectormap.cpp
+++ tests/vectormap.cpp
@@ -9,10 +9,13 @@ using namespace OrbitSets;
 
 int main() {
 	eqimap<vector<rational>,vector<rational>> test;
-	test.add(vector<rational>({rational(1)}),vector<rational>({rational(1)}));
+	test.add(vector<rational>({rational(2), rational(1)}),vector<rational>({rational(2)}));
 	
 	for (auto kv : test) {
-		cout << kv.first.getElement() << " " << kv.second.first.getElement() << endl;
+		cout << kv.first.getElement() << " " << kv.second.first.getElement() << " ";
+		for (auto b : kv.second.second)
+			cout << b;
+		cout << endl;
 	}
 	return 0;
 }
