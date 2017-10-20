5bb2bd3db72d6fffd974f0a22aed8a040665882f
David Venhoek <david@venhoek.nl>
2017-10-20 20:54:20 +0200

    Added sample used in paper presenting ONS.

---
+++ samples/article.cpp
@@ -0,0 +1,24 @@
+#include <OrbitSets/rational.h>
+#include <OrbitSets/orbit.h>
+#include <OrbitSets/nomset.h>
+#include <iostream>
+
+using namespace std;
+using namespace OrbitSets;
+
+int main() {
+	// Create the nominal set with all rational numbers.
+	nomset<rational> A = nomset_rationals();
+
+	// Create the orbit and nomset for B, and insert the orbit into the nomset.
+	orbit<pair<rational,rational>> B_orbit(make_pair(rational(1), rational(2)));
+	nomset<pair<rational,rational>> B; // The default set is empty.
+	B.insert(B_orbit);
+
+	// Calculate the product
+	auto AtimesB = nomset_product(A,B);
+
+	// And print elements of each orbit
+	for (auto orbit : AtimesB)
+		cout << orbit.getElement() << endl;
+}
