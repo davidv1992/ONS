6ffebca0b3e4d0802b255c316b594a474dda5142
David Venhoek <david@venhoek.nl>
2018-03-20 13:00:27 +0100

    Fixed article.cpp being out of sync.

--- samples/article.cpp
+++ samples/article.cpp
@@ -7,18 +7,10 @@ using namespace std;
 using namespace ONS;
 
 int main() {
-	// Create the nominal set with all rational numbers.
 	nomset<rational> A = nomset_rationals();
+	nomset<pair<rational, rational>> B({rational(1),rational(2)});
 
-	// Create the orbit and nomset for B, and insert the orbit into the nomset.
-	orbit<pair<rational,rational>> B_orbit(make_pair(rational(1), rational(2)));
-	nomset<pair<rational,rational>> B; // The default set is empty.
-	B.insert(B_orbit);
-
-	// Calculate the product
-	auto AtimesB = nomset_product(A,B);
-
-	// And print elements of each orbit
+	auto AtimesB = nomset_product(A, B);   // compute the product
 	for (auto orbit : AtimesB)
 		cout << orbit.getElement() << endl;
 }
